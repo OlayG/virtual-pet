3e6fd849d64d82061cc68f3b92285a42017131de
OlayG <ogureje@yahoo.com>
2016-06-27 16:00:15 -0400

    more updates

--- VirtualPet/Temperment.cs
+++ VirtualPet/Temperment.cs
@@ -8,17 +8,14 @@ namespace VirtualPet
 {
     class Temperment
     {
-
+        Pet Pug = new Pet();
         public static void CharmingPug()
         {
             //Pugs are naturally beautiful dogs. They are small with an adorable face that will make anyone want to hold and cuddle them. 
             //Of course they know this and will use every opportunity they have to charm you.
-            Console.WriteLine("Pug is bad");
+            
+            Console.WriteLine("{0} says gimmi food you fool!\nbut you see it as a cute jesture for food");
         }
-
-        //public void PlayfulPug()
-        //{
-        //}
 
         public void DocilePug()
         {
@@ -30,30 +27,22 @@ namespace VirtualPet
         public void CleverPug()
         {
             //By being fairly smart dogs, you can expect your pug to be pretty clever.
-            Console.WriteLine("Pug is sad");
+            Console.WriteLine("Something");
         }
-
-        //public void QuietPug()
-        //{
-        //}
 
         public void StubbornPug()
         {
             //Having a natural stubborn personality, most pugs will do what they want to do and 
             //if they do not receive the proper training it can be hard to change their mind.
-            Console.WriteLine("Pug is sad");
+            Console.WriteLine("{0} is getting tired of these snacks wants the ones scooby has");
         }
-
-        //public void AttentivePug()
-        //{
-        //}
 
         public void SociablePug()
         {
             //You never want your Pug to be in complete isolation. 
             //Leaving for work and such is ok, but to leave him unattended for long periods of time will put a lot of stress on him. 
             //He is a social creature that will always seek to be with someone.
-            Console.WriteLine("Pug is sad");
+            Console.WriteLine("{0} is feeling extra good today showing you more affection than usual");
         }
     }
 }
